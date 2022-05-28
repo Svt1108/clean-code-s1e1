db34afbf974718f302e71dcaa6f8c93bb4230c0e
Svetlana Pozina <cherryblack@list.ru>
2022-05-28 10:34:18 +0300

    refactor: change class name according to rule 3.3 in html-and-css.md v2

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.classList.add('button', 'items__button_delete');
     deleteButtonImg.src='./remove.svg';
-    deleteButtonImg.className='button__delete-img';
+    deleteButtonImg.className='button__img';
     deleteButton.appendChild(deleteButtonImg);
 
 
